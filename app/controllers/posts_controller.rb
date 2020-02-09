0a9d8e0e2c0d2696bff5b18729d6d1ee1de39c3f
shadowfox3735 <normanpan180@gmail.com>
2020-02-09 02:35:10 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,6 @@ class PostsController < ApplicationController
 	private
  
   def post_params(*args)
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(*args)
   end
 end
